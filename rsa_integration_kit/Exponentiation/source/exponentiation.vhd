1c58dc72ca0c9e3843524d601be6cb65f862f07a
Carl Richard <crfosse@stud.ntnu.no>
2019-11-04 20:52:48 +0100

    Further work on main FSM

--- rsa_integration_kit/Exponentiation/source/exponentiation.vhd
+++ rsa_integration_kit/Exponentiation/source/exponentiation.vhd
@@ -32,17 +32,22 @@ end exponentiation;
 
 
 architecture expBehave of exponentiation is
-begin
 	
-	exp_fsm: process ()
+	signal start_exp : in STD_LOGIC();
+
+
+begin
+	exp_fsm: process (PS, start_exp)
 		begin
 			case PS is 
+				m_start <= '0';
+				p_start <= '0';
 				when IDLE
-
-					if start = '1' then
-							NS <= PRECOMPUTE_INIT;
+					
+			            if start_exp = '1' then
+					NS <= PRECOMPUTE_INIT;
 				    end if;
-
+					
 				when PRECOMPUTE_INIT =>
 					-- Initialize and start/reset partitioner and counters.
 					-- Ensure that neccesary signals(p_start, counter1=0) is set.
@@ -54,14 +59,53 @@ begin
 					-- Load registers with message(chosen with a mux)
 					-- Set PMEM[counter1] = M (initial message)
 
-				    -- Conditions for ensuring this is finished?
-				when PRECOMPUTE =>
+				        -- Conditions for ensuring this is finished?
+
+					NS <= PRECOMPUTE_COMPUTE;
+				when PRECOMPUTE_MODMULT =>
 					-- Start modular multiplier. If it's finished, set C on the output. 
+					m_start <= '1'; --
+					if(m_finished) then
+						-- Increment counter
+						-- Set PMEM[counter1] = R(Current result)
+						-- Load A
+						NS <= CHECK_POWER;
+					else 
+						NS <= PRECOMPUTE;
+				        end if;
+				when PRECOMPUTE_CHECK_POWER =>
+					-- Check if the counter1 equals 2^(d-1)-1. (we can also increment counter1 by 2, but I don't know if wheter that is smart or not)
+					-- We can always check this maybe? 
+					if(equal = '1') then
+						if(p_finished) then
+							NS<=EXP_START;
+							read_partitioner <= 1;
+						end if;
+					else
+						NS <= PRECOMPUTE_MOD_MULT;
+					end if;
+				when EXP_READ_PARTITIONER =>
+					read_power_memory <=1;
+					counter1 <= word.value; -- The value of the current word
+					counter2 <= word.length; -- The length of the current word(applicable if zero word) 
+				when EXP_CHECK_COUNTERS =>
+					-- LOAD A
+					-- LOAD B
+					if(zero_counter_2 = '0') then 
+						NS => EXP_LD_ONLY_B;
+					end if;
+					if(zero_partitioner = '0') then
+						NS => EXP_FINISHED;
+					end if;
+					
+
+
+				 	 
+					    
+						
 			end case;
 
 	end exp_fsm;
-
-
 
 	result <= message xor modulus;
 	ready_in <= ready_out;
